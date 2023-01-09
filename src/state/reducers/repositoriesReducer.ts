8c69ccb8df7cf00a67a2524e109e3932c8c36dfd
KrysDev <webdevkrys@gmail.com>
2023-01-09 16:44:48 +0000

    x

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,15 @@
+interface RepositoriesState {
+  loading: string;
+  error: boolean | null;
+  data: string[];
+}
+
+// interface RepositoriesAction {
+
+// }
+
+const reducer = (state: RepositoriesState, action: any) => {
+
+};
+
+export default reducer;
