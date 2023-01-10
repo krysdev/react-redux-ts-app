caf2f42e483a428d022ba81347b61ecc5545c42a
KrysDev <webdevkrys@gmail.com>
2023-01-10 21:30:53 +0000

    x

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,15 +1,48 @@
 interface RepositoriesState {
-  loading: string;
-  error: boolean | null;
+  loading: boolean;
+  error: string | null;
   data: string[];
 }
 
-// interface RepositoriesAction {
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
 
-// }
+interface SearchRepositoriesSuccesAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
 
-const reducer = (state: RepositoriesState, action: any) => {
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
 
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccesAction
+  | SearchRepositoriesErrorAction;
+
+enum ActionType{
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
